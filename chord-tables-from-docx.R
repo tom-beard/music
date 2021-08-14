eec5034e364c0a88b09e22a45a6efaa985b18f28
Tom <wellurban@gmail.com>
2021-08-14 17:54:43 +1200

    Read release notes docx

--- chord-tables-from-docx.R
+++ chord-tables-from-docx.R
@@ -7,3 +7,9 @@ library(glue)
 library(skimr)
 library(janitor)
 library(officer)
+
+docx_path <- path("D:", "modular", "firmware", "E352_V16", "E370 Release Notes V16.docx")
+
+docx_obj <- read_docx(docx_path)
+
+str(docx_obj)
